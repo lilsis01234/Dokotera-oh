535063a53fdef3a722f0a41ee7a78f6ea1847361
mandresy <mandresyrakotonanahary@gmail.com>
2023-09-14 12:09:58 +0100

    Card doctor

--- front/App.js
+++ front/App.js
@@ -1,12 +1,14 @@
 import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { CardList } from "./components/CardList/CardList";
+import { CardDoctor } from "./components/CardDoctor/CardDoctor";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
         <Text>Eto mi-affiche anle components</Text>
+        <CardDoctor/>
         <CardList />
       </SafeAreaView>
     </SafeAreaProvider>
