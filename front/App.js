036aac8e2bab5479bc0c28ca0ca0f624df7a095f
Tolotra <tolotrarakototsiafa@gmail.com>
2023-09-14 12:04:22 +0300

    add CardRdv

--- front/App.js
+++ front/App.js
@@ -1,11 +1,13 @@
 import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import CardRdv from "./components/CardRdv/CardRdv";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
         <Text>Eto mi-affiche anle components</Text>
+         <CardRdv />
       </SafeAreaView>
     </SafeAreaProvider>
   );
