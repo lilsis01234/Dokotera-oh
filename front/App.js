f301b4521bf9c6f2223c574ae5ceb128683ef9d9
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-14 12:11:22 +0300

    feat: create Header component

--- front/App.js
+++ front/App.js
@@ -1,11 +1,13 @@
 import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Header } from "./components/Header/Header";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
         <Text>Eto mi-affiche anle components</Text>
+        <Header/>
       </SafeAreaView>
     </SafeAreaProvider>
   );
