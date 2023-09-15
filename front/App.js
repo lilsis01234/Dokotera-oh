8ed0b20e3256e795b32df052b3cdaa316485c55a
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-15 11:27:57 +0300

    installation initialisation tailwind

--- front/App.js
+++ front/App.js
@@ -1,20 +1,26 @@
-import { Text } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import CardRdv from "./components/CardRdv/CardRdv";
-import { Header } from "./components/Header/Header";
-import { CardList } from "./components/CardList/CardList";
-import { CardDoctor } from "./components/CardDoctor/CardDoctor";
+// import CardRdv from "./components/CardRdv/CardRdv";
+// import { Header } from "./components/Header/Header";
+// import { CardList } from "./components/CardList/CardList";
+// import { CardDoctor } from "./components/CardDoctor/CardDoctor";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Eto mi-affiche anle components</Text>
-         <CardRdv />
-        <CardDoctor/>
-        <Header/>
-        <CardList />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      {/* <SafeAreaProvider> */}
+        {/* <SafeAreaView> */}
+          {/* <Text>Eto mi-affiche anle components</Text> */}
+          {/* <CardRdv /> */}
+          {/* <CardDoctor/> */}
+          {/* <CardList /> */}
+          {/* <Header/> */}
+        {/* </SafeAreaView> */}
+      {/* </SafeAreaProvider> */}
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
--- front/components/Header/Header.jsx
+++ front/components/Header/Header.jsx
@@ -1,6 +1,11 @@
-import { Text } from "react-native";
+import { Text, View, StatusBar} from "react-native";
 import { s } from "./Header.style";
 
 export function Header() {
-  return <Text>Hello Header</Text>;
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
