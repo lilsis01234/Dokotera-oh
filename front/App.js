26201438a4274efc99767e1f382952a62a76edb2
Tolotra <tolotrarakototsiafa@gmail.com>
2023-09-15 18:04:55 +0300

    change app.js

--- front/App.js
+++ front/App.js
@@ -4,17 +4,17 @@ import CardRdv from "./components/CardRdv/CardRdv";
 import { Header } from "./components/Header/Header";
 import { CardList } from "./components/CardList/CardList";
 import { CardDoctor } from "./components/CardDoctor/CardDoctor";
-import { s } from "./components/CardDoctor/CardDoctor.style";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={s.form}>
-        <CardDoctor
-          firstname={"mandresy"}
-          name={"santatsoa"}
-          specialité={"généraliste"}
-        />
+      <SafeAreaView>
+          <View>
+          <CardDoctor/>
+          <CardRdv/>
+          <CardList/>
+          <Header/> 
+          </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
