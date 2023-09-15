4c93345b0b6a3c1b9344b5129733ef9dc64cdb52
Hardy Sylvestre <hardsyldev@gmail.com>
2023-09-15 09:08:03 +0300

    a conserver

--- front/App.js
+++ front/App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello Wolrd ngiqah</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
