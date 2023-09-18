1237452c17d1d3fc56e5ad99d3caea817388e6ce
Tolotra <tolotrarakototsiafa@gmail.com>
2023-09-18 13:15:16 +0300

    adc

--- front/App.js
+++ front/App.js
@@ -1,39 +1,14 @@
 import { ImageBackground, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import CardRdv from "./components/CardRdv/CardRdv";
+import {CardRdv} from "./components/CardRdv/CardRdv";
 import { Header } from "./components/Header/Header";
-import { CardList } from "./components/CardList/CardList";
 import { CardDoctor } from "./components/CardDoctor/CardDoctor";
 import { ListRdv } from "./components/CardRdv/ListRdv";
 import { s } from "./components/CardDoctor/CardDoctor.style";
 import { Register } from "./components/pages/Register";
 import AppointmentList from "./components/CardRdv/ItemRdv";
-
-const appointments = [
-  { id: 1, date: '2023-09-20', time: '10:00 AM', description: 'Cabinet du médecin', name: 'Rendez-vous médical' },
-  { id: 2, date: '2023-09-21', time: '3:00 PM', description: 'Bureau', name: 'Réunion de travail' },
-  // Ajoutez d'autres rendez-vous ici
-];
-
-export default function App() {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-      <AppointmentList appointments={appointments} />
-      </SafeAreaView>
-    </SafeAreaProvider>
-import { ProfilDoctor } from "./components/ProfilDoctor/ProfilDoctor";
 import Sary from "./assets/fond.jpg";
-
-export default function App() {
-  return (
-    <ImageBackground source={Sary} style={s.form}>
-      <SafeAreaProvider>
-        <SafeAreaView style={s.form}>
-          <ProfilDoctor />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </ImageBackground>
+import { ProfilDoctor } from "./components/ProfilDoctor/ProfilDoctor";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -44,12 +19,19 @@ import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/Login";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Stack = createNativeStackNavigator();
+const appointments = [
+  { id: 1, date: '2023-09-20', time: '10:00 AM', description: 'Cabinet du médecin', name: 'Rendez-vous médical' },
+  { id: 2, date: '2023-09-21', time: '3:00 PM', description: 'Bureau', name: 'Réunion de travail' },
+  // Ajoutez d'autres rendez-vous ici
+];
 
 export default function App() {
   return (
+
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="ListRdv" component={AppointmentList} appointments={appointments}/>
+      <Stack.Screen name="CardRdv" component={CardRdv} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen
           name="RegistrationScreen"
