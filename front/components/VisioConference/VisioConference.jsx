64546513f8b90698a8db628d06a46e3bf43d96be
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-19 11:36:02 +0300

    visioconférence

--- front/components/VisioConference/VisioConference.jsx
+++ front/components/VisioConference/VisioConference.jsx
@@ -38,9 +38,12 @@ const AppointmentsScreen = ({ route }) => {
           audio: true, // You can include audio if needed
         });
         setLocalStream(stream);
+      } else {
+        console.log("Permission not granted for camera");
+        // Handle the case where permission is not granted, e.g., show a message to the user
       }
     };
-
+  
     getCameraPermission();
   }, []);
 
