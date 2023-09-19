90b4b4231ff10bd7c00047596b0ea282b2a9164c
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-19 11:57:20 +0300

    visioconference

--- front/components/VisioConference/VisioConference.jsx
+++ front/components/VisioConference/VisioConference.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 import { Video } from 'expo-av';
+import axios from 'axios';
 
 const AppointmentsScreen = ({ route }) => {
   const [docteur,setDocteur]= useState([]);
